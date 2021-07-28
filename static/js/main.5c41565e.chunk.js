57425d6b4c0ab651fa81a6c99e1af2a20c70eb41
SauloSCorrea <saulo.correa@ibti.org.br>
2021-07-28 15:32:25 -0300

    Updates

---
+++ static/js/main.5c41565e.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonptreinamento=this.webpackJsonptreinamento||[]).push([[0],{10:function(e,n,t){"use strict";t.r(n);t(1);var o=t(3),r=t.n(o),c=(t(8),t(0)),i=function(){return Object(c.jsx)("div",{children:"Hello World!!!"})};r.a.render(Object(c.jsx)(i,{}),document.getElementById("root"))},8:function(e,n,t){}},[[10,1,2]]]);
+//# sourceMappingURL=main.5c41565e.chunk.js.map
